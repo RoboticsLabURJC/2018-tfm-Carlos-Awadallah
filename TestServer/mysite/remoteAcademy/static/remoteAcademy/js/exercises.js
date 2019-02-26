648a5318d9eec91ec8ae695abaead17c3231b859
Carlos Awadallah <carlosawadallah@gmail.com>
2019-02-26 12:22:38 +0100

    reorganization

--- TestServer/mysite/remoteAcademy/static/remoteAcademy/js/exercises.js
+++
@@ -1,9 +0,0 @@
-function displayModal() {
-    var modal = document.getElementById('myModal');
-    modal.style.display = "block";
-}
-
-function closeModal() {
-    var modal = document.getElementById('myModal');
-    modal.style.display = "none";
-}
